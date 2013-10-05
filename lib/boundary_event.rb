f0ccf53987a2ce7d5842b399a6500c79bc54912a
Nolan Davidson <nolan.davidson@gmail.com>
2013-10-05 10:17:22 -0400

    Implemented initialize method

--- lib/boundary_event.rb
+++ lib/boundary_event.rb
@@ -1,5 +1,10 @@
 require "boundary_event/version"
 
-module BoundaryEvent
-  # Your code goes here...
+class BoundaryEvent
+	attr_accessor :api_key, :org_id
+	
+	def initialize(params = {})
+		@api_key = params.fetch(:api_key, nil)
+		@org_id = params.fetch(:org_id, nil)
+	end
 end
--- lib/boundary_event/version.rb
+++ lib/boundary_event/version.rb
@@ -1,3 +1,3 @@
-module BoundaryEvent
+class BoundaryEvent
   VERSION = "0.0.1"
 end
