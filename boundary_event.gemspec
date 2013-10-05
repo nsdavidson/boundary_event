8e0b53476d595b98c938a33fe37e49021a8b9a73
Nolan Davidson <nolan.davidson@gmail.com>
2013-10-05 12:08:31 -0400

    Added initial data attributes and populate_event method

--- boundary_event.gemspec
+++ boundary_event.gemspec
@@ -21,9 +21,5 @@ Gem::Specification.new do |spec|
   spec.required_ruby_version = ">= 1.8.7"
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
-  spec.add_dependency "base64"
   spec.add_dependency "json"
-  spec.add_dependency "net/http"
-  spec.add_dependency "net/https"
-  spec.add_dependency "uri"
 end
--- lib/boundary_event.rb
+++ lib/boundary_event.rb
@@ -1,16 +1,21 @@
 require "boundary_event/version"
 
-require "base64"
-require "json"
-require "net/http"
-require "net/https"
-require "uri"
-
 class BoundaryEvent
+	# Attributes to hold credential data
 	attr_accessor :api_key, :org_id
+
+	# Attributes to hold event data
+	attr_accessor :title, :tags, :status, :severity, :message, :properties, :source, :sender, :fingerprint_fields
 	
 	def initialize(params = {})
 		@api_key = params.fetch(:api_key, nil)
 		@org_id = params.fetch(:org_id, nil)
+		populate_event(params)
+	end
+
+	def populate_event(params = {})
+		@title = params.fetch(:title, nil) unless !params.has_key?(:title)
+		@tags = params.fetch(:tags, nil)
+		@status = params.fetch(:status, nil)
 	end
 end
